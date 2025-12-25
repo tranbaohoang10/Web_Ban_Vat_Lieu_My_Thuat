9eeb6cfaf07f30e24e001030318d77e834c6e478
danghau2512 <23130095@st.hcmuaf.edu.vn>
2025-12-25 11:05:02 +0700

    Cập nhật gitignore giao hang nhanh

---
+++ Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Controller/VoucherController.java
@@ -0,0 +1,10 @@
+package vn.edu.nlu.fit.mythuatshop.Controller; import jakarta.servlet.*; 
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException; 
+@WebServlet(name = "VoucherController", value = "/VoucherController") 
+public class VoucherController extends HttpServlet { 
+
+@Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+@Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+}
