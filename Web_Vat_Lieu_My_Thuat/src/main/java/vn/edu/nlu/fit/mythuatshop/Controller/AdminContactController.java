efe4bc9bfd7acce1746e061d2911bf4ac0212d87
danghau2512 <23130095@st.hcmuaf.edu.vn>
2025-12-25 22:04:31 +0700

    Load dữ liệu từ database và dùng datatable để hiển thị dữ liệu

---
+++ Web_Vat_Lieu_My_Thuat/src/main/java/vn/edu/nlu/fit/mythuatshop/Controller/AdminContactController.java
@@ -0,0 +1,10 @@
+package vn.edu.nlu.fit.mythuatshop.Controller; import jakarta.servlet.*; 
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException; 
+@WebServlet(name = "AdminContactController", value = "/AdminContactController") 
+public class AdminContactController extends HttpServlet { 
+
+@Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+@Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { } 
+}
